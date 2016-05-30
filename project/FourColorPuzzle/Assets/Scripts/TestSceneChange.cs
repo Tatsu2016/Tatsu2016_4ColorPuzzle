b935502ebd7325b3d359c2463cbdbedd839d944f
Tatsu2016 <newtatsu2016@gmail.com>
2016-05-31 00:10:51 +0900

    佐藤
    パズルの種類が増えたので
    テスト用にシーン切り替え実装

---
+++ project/FourColorPuzzle/Assets/Scripts/TestSceneChange.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TestSceneChange : MonoBehaviour
+{
+	public void ButtonPush(string text)
+	{
+		SceneManager.LoadScene(text);
+	}
+}
