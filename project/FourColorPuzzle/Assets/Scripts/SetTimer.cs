9a4298b5454f55d396aed547be63c2bca0950b86
Tatsu2016 <newtatsu2016@gmail.com>
2016-06-12 20:55:07 +0900

    髙下　パズル画面での時間表示と、最短記録をシーン選択画面で表示させた

---
+++ project/FourColorPuzzle/Assets/Scripts/SetTimer.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SetTimer : MonoBehaviour {
+    private int minute;
+    private float second;
+    private int oldsecond;
+    public Text timeLabel;
+    public ClearCheck clear;
+    public string minutekey;
+    public string secondkey;
+
+	// Use this for initialization
+	void Start () {
+        minute = 0;
+        second = 0;
+        oldsecond = 0;
+
+  //      PlayerPrefs.DeleteAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale > 0 && clear.isClear == false)
+        {
+            second += Time.deltaTime;
+            if (second >= 60.0f)
+            {
+                second = 0;
+                minute++;
+            }
+            if ((int)second != oldsecond)
+                timeLabel.text = minute.ToString("00") + ":" + second.ToString("00");
+
+            oldsecond = (int)second;
+
+            //Debug.Log(PlayerPrefs.HasKey(minutekey));
+        }
+
+        if (clear.isClear == true)
+        {
+            if (PlayerPrefs.HasKey(minutekey) == false)
+            {
+                PlayerPrefs.SetInt(minutekey, minute);
+            }
+            if (PlayerPrefs.HasKey(secondkey) == false)
+            {
+                PlayerPrefs.SetInt(secondkey, (int)second);
+            }
+
+            if (PlayerPrefs.GetInt(minutekey) > minute)
+            {
+                PlayerPrefs.SetInt(minutekey, minute);
+            }
+            if (PlayerPrefs.GetInt(secondkey) > (int)second)
+            {
+                PlayerPrefs.SetInt(secondkey, (int)second);
+            }
+
+        }
+    }
+
+    //void OnGUI()
+    //{
+    //    if (GUI.Button(new Rect(0, 0, 150, 100), "reset"))
+    //    {
+    //        //デバッグ用。保存データ初期化
+    //        PlayerPrefs.DeleteAll();
+    //        Debug.Log("データ全消去したわよッ!!(・∀・)");
+    //    }
+    //}
+}
---
+++ project/FourColorPuzzle/Assets/Scripts/ShortestRecord.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShortestRecord : MonoBehaviour {
+    public Text recordLabel;
+    public string minutekey;
+    public string secondkey;
+
+	// Use this for initialization
+	void Start () {
+        recordLabel.text = PlayerPrefs.GetInt(minutekey).ToString("00") + ":" + PlayerPrefs.GetInt(secondkey).ToString("00");
+        //Debug.Log(PlayerPrefs.HasKey(minutekey));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+#if UNITY_EDITOR_WIN
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 200, 100, 50), "reset"))
+        {
+            //デバッグ用。保存データ初期化
+            PlayerPrefs.DeleteAll();
+            Debug.Log("データ全消去!!!(・∀・)");
+        }
+    }
+#endif
+}
